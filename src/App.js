292af9eedd49a9d061a9b26f8c7ceec2b5176f73
varionabil <varionabilr@gmail.com>
2020-06-23 00:49:00 +0700

    yeah right

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 function App() {
+  const handleClick = (e) => {
+    alert("Post-mo sucks!!!");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        <img src={require('./profile.jpg')} alt="" />
+        <h1>[variousnabil]</h1>
+        <button onClick={handleClick}>Yell</button>
+      </p>
     </div>
   );
 }
